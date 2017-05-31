25b737cad7a8589aa1157366f1b5e8ca812f2fd6
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-05-31 21:57:39 +0900

    Fix 'atcoder start' script

--- tools/atcoder.js
+++ tools/atcoder.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const spawn = require('child_process').spawn
 
-const httpcli = require('cheerio-httpcli')
+const cheerio = require('cheerio')
 
 const base = path.resolve(path.dirname(process.argv[1]), '..')
 
@@ -90,26 +90,22 @@ function generateMock (contestName, problems) {
 }
 
 function downloadTestCases (contestName) {
-    return new Promise((resolve, reject) => {
-        httpcli.fetch(`http://${contestName}.contest.atcoder.jp/assignments`, (err, $) => {
-            if (err) {
-                reject(err)
-            } else {
-                let results = []
-                $('#outer-inner table tbody tr td.center a').each(function (idx, elem) {
-                    const problemName = $(this).text().toLowerCase()
-                    const link = $(this).attr('href')
+    return exec('python3', [path.resolve(path.dirname(process.argv[1]), 'allproblems.py'), `http://${contestName}.contest.atcoder.jp/assignments`])
+    .then(data => {
+        const $ = cheerio.load(data)
+        let results = []
+        $('#outer-inner table tbody tr td.center a').each(function (idx, elem) {
+            const problemName = $(this).text().toLowerCase()
+            const link = $(this).attr('href')
 
-                    const result = execWithOutput('oj', [
-                        'dl',
-                        '-f', path.resolve(base, 'contest', contestName, `test-${problemName}`, 'sample%i.%e'),
-                        `http://${contestName}.contest.atcoder.jp${link}`
-                    ])
-                    results.push(result)
-                })
-                Promise.all(results).then(() => { resolve() })
-            }
+            const result = execWithOutput('oj', [
+                'dl',
+                '-f', path.resolve(base, 'contest', contestName, `test-${problemName}`, 'sample%i.%e'),
+                `http://${contestName}.contest.atcoder.jp${link}`
+            ])
+            results.push(result)
         })
+        return Promise.all(results)
     })
 }
 
