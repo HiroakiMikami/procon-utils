5bd74cbbe1be281baa65ef95a07e22dbae072bba
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-06-17 22:19:50 +0900

    Use std::tie in main

--- template/template.cc
+++ template/template.cc
@@ -406,6 +406,7 @@ int body(int &argc, char **argv);
 
 // main function (DO NOT EDIT)
 int main (int argc, char **argv) {
+    cin.tie(0);
     ios_base::sync_with_stdio(false);
     return body(argc, argv);
 }
