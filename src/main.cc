e9ce9cfdb1534a7ca40a5c6f78796152c3b733f4
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-07-17 16:49:38 +0900

    Add main file (template)

---
+++ src/main.cc
@@ -0,0 +1,12 @@
+int body(int &argc, char **argv);
+
+// main function (DO NOT EDIT)
+int main (int argc, char **argv) {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    return body(argc, argv);
+}
+
+int body(int &argc, char **argv) {
+    return 0;
+}
