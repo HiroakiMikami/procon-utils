52806b9629ab75c27176d62fb511d8e4deef449d
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-07-21 19:19:56 +0900

    Use size_t as the element type of union find

--- test/data-structure/test_union_find.cc
+++ test/data-structure/test_union_find.cc
@@ -4,32 +4,21 @@
 #include "data-structure/union_find.cc"
 
 TEST(UnionFindTest, ConstructorTest) {
-    union_find<string> f({"x", "y", "s", "t"});
-    EXPECT_TRUE(f.is_same("x", "x"));
-    EXPECT_FALSE(f.is_same("x", "y"));
-    EXPECT_FALSE(f.is_same("x", "s"));
-    EXPECT_FALSE(f.is_same("x", "t"));
-}
-TEST(UnionFindTest, MergeTest) {
-    union_find<string> f({"x", "y", "s", "t"});
-
-    f.merge("x", "y");
-    f.merge("s", "t");
-    EXPECT_TRUE(f.is_same("x", "x"));
-    EXPECT_TRUE(f.is_same("x", "y"));
-    EXPECT_FALSE(f.is_same("x", "s"));
-
-    f.merge("y", "t");
-    EXPECT_TRUE(f.is_same("x", "s"));
-}
-TEST(UnionFindTest, TemplateSpecializationTest) {
-    union_find<size_t> f(4);
+    union_find f(4);
     EXPECT_TRUE(f.is_same(0, 0));
     EXPECT_FALSE(f.is_same(0, 1));
+    EXPECT_FALSE(f.is_same(0, 2));
     EXPECT_FALSE(f.is_same(0, 3));
+}
+TEST(UnionFindTest, MergeTest) {
+    union_find f(4);
 
     f.merge(0, 1);
+    f.merge(2, 3);
     EXPECT_TRUE(f.is_same(0, 0));
     EXPECT_TRUE(f.is_same(0, 1));
-    EXPECT_FALSE(f.is_same(0, 3));
+    EXPECT_FALSE(f.is_same(0, 2));
+
+    f.merge(1, 3);
+    EXPECT_TRUE(f.is_same(0, 2));
 }
