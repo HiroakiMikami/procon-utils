18977ef71ec1c7d798e1d1d9fa80254558723dae
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-08-12 17:47:23 +0900

    Add util/mod

---
+++ src/util/mod.cc
@@ -0,0 +1,112 @@
+#ifndef MAIN
+#include "common.cc"
+#include "math.cc"
+#endif
+
+namespace mod {
+    const i64 MOD = 1000000007;
+
+    struct Integer {
+        Integer() : value(0) {}
+        Integer(i64 value) {
+            update_value((value));
+        }
+
+        Integer inverse() const {
+            return math::pow(*this, 1000000005);
+        }
+
+        Integer operator+(const Integer &rhs) const {
+            return this->value + rhs.value;
+        }
+        Integer operator-(const Integer &rhs) const {
+            return this->value - rhs.value;
+        }
+
+        Integer operator*(const Integer &rhs) const {
+            return this->value * rhs.value;
+        }
+
+        Integer operator /(const Integer &rhs) const {
+            return this->value * rhs.inverse().value;
+        }
+
+        void operator +=(const Integer &rhs) {
+            update_value(this->value + rhs.value);
+        }
+
+        void operator -=(const Integer &rhs) {
+            update_value(this->value - rhs.value);
+        }
+
+        void operator *=(const Integer &rhs) {
+            update_value(this->value * rhs.value);
+        }
+
+        void operator /=(const Integer &rhs) {
+            update_value(this->value * rhs.inverse().value);
+        }
+
+        bool operator==(const Integer &rhs) const {
+            return this->value == rhs.value;
+        }
+        template <class V>
+        bool operator==(V rhs) const {
+            return this->value == rhs;
+        }
+
+        bool operator!=(const Integer &rhs) const {
+            return this->value != rhs.value;
+        }
+        template <class V>
+        bool operator!=(V rhs) const {
+            return this->value != rhs;
+        }
+
+        const i64 &get() const {
+            return this->value;
+        }
+
+    private:
+        void update_value(i64 new_value) {
+            this->value = new_value;
+            if (this->value < 0) {
+                this->value += MOD;
+            }
+            if (this->value >= MOD) {
+                this->value %= MOD;
+            }
+        }
+        i64 value;
+    };
+
+    template <class V>
+    static bool operator==(V lhs, const Integer &rhs) {
+        return rhs ==lhs;
+    }
+    template <class V>
+    static bool operator!=(V lhs, const Integer &rhs) {
+        return rhs != lhs;
+    }
+
+    struct fact {
+        Integer value;
+        Integer inverse;
+    };
+
+    static V<fact> fact_table(const size_t n) {
+        V<fact> retval(n + 1);
+
+        retval[0].value = 1;
+        FOR(i, 1, n + 1) {
+            retval[i].value = retval[i - 1].value * i;
+        }
+
+        retval[n].inverse = retval[n].value.inverse();
+        REPR(i, n) {
+            retval[i].inverse = retval[i + 1].inverse * (i + 1);
+        }
+
+        return retval;
+    }
+}
---
+++ test/util/test_mod.cc
@@ -0,0 +1,54 @@
+#define MAIN
+
+#include <gtest/gtest.h>
+#include "common.cc"
+#include "util/math.cc"
+#include "util/mod.cc"
+
+using namespace mod;
+
+TEST(ModTest, ConstructorTest) {
+    EXPECT_EQ(0, Integer().get());
+    EXPECT_EQ(MOD - 1, Integer(-1).get());
+    EXPECT_EQ(1, Integer(MOD + 1).get());
+}
+
+TEST(ModTest, InverseTest) {
+    Integer x(10);
+    auto y = x.inverse();
+    EXPECT_EQ(1, x * y);
+}
+
+TEST(ModTest, PlusTest) {
+    EXPECT_EQ(0, Integer(MOD)+MOD);
+}
+TEST(ModTest, MinusTest) {
+    EXPECT_EQ(1, Integer(1) - MOD);
+}
+TEST(ModTest, Multiplytest) {
+    EXPECT_EQ(Integer(-2), Integer(MOD-1) * 2);
+}
+TEST(ModTest, DivideTest) {
+    EXPECT_EQ(Integer(2), Integer(20) / 10);
+}
+
+TEST(ModTest, CombinationTest) {
+    EXPECT_EQ(Integer(4), math::combination(Integer(4), 1));
+}
+
+TEST(ModTest, FactTableTest) {
+    auto table = fact_table(3);
+    EXPECT_EQ(4, table.size());
+
+    EXPECT_EQ(1, table[0].value);
+    EXPECT_EQ(1, table[0].inverse);
+
+    EXPECT_EQ(1, table[1].value);
+    EXPECT_EQ(1, table[1].inverse);
+
+    EXPECT_EQ(2, table[2].value);
+    EXPECT_EQ(Integer(2).inverse(), table[2].inverse);
+
+    EXPECT_EQ(6, table[3].value);
+    EXPECT_EQ(Integer(6).inverse(), table[3].inverse);
+}
