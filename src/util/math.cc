7c745cacb56a2f2aff3d79cb97f9331d5d15e9a5
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-08-12 17:46:41 +0900

    Make pow and combination template

--- src/util/math.cc
+++ src/util/math.cc
@@ -6,7 +6,8 @@ namespace math {
     /*
      * n^r
      */
-    i64 pow(i64 n, i64 r) {
+    template <class V>
+    static V pow(V n, i64 r) {
         if (r == 0) {
             return 1;
         }
@@ -17,17 +18,18 @@ namespace math {
         return x2 * x2 * ((r % 2 == 0) ? 1 : n);
     }
 
-    i64 gcd(i64 a, i64 b) {
+    static i64 gcd(i64 a, i64 b) {
         if (b == 0) return a;
         return gcd(b, a % b);
     }
 
-    i64 combination(i64 n, i64 r) {
+    template <class V>
+    static V combination(V n, i64 r) {
         if (r == 0) {
             return 1;
         }
 
-        i64 x = 1;
+        V  x = 1;
         FOR (d, 1, r + 1) {
             x *= n;
             n -= 1;
