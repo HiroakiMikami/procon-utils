ccc1c8e3dae466ca787b9a2edab906dc8cbffc73
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2018-01-02 12:21:31 +0900

    Implement BFS

--- src/algorithm/traverse.cc
+++ src/algorithm/traverse.cc
@@ -40,3 +40,39 @@ void dfs(const Graph &g, const std::vector<size_t> &start,
                                     return retval;
                                 });
 }
+
+template <class Graph, class F1, class F2>
+void bfs_with_duplicate_vertices(const Graph &g, const std::vector<size_t> &start,
+                                 const F1 &process, const F2 &is_added) {
+    using _Edge = Edge<typename Graph::EdgeLabel>;
+    std::queue<_Edge> s;
+    for (const auto &n: start) {
+        for (auto edge: g.outgoings(n)) {
+            if (is_added(edge)) s.push(edge);
+        }
+    }
+
+    while (!s.empty()) {
+        auto x = s.front();
+        s.pop();
+
+        if (process(x)) {
+            return ;
+        }
+
+        for (const auto edge: g.outgoings(get<1>(x))) {
+            if (is_added(edge)) s.push(edge);
+        }
+    }
+}
+template <class Graph, class F1, class F2>
+void bfs(const Graph &g, const std::vector<size_t> &start,
+         const F1 &process, const F2 &is_added) {
+    std::vector<bool> is_visited(g.vertices_size(), false);
+    bfs_with_duplicate_vertices(g, start, process,
+                                [&is_added, &is_visited](const auto &edge) {
+                                    auto retval = !is_visited[get<1>(edge)];
+                                    is_visited[get<1>(edge)] = true;
+                                    return retval;
+                                });
+}
--- test/algorithm/test_traverse.cc
+++ test/algorithm/test_traverse.cc
@@ -21,8 +21,25 @@ struct DfsTest : public ::testing::Test {
     }
 };
 
+template <class Graph>
+struct BfsTest : public ::testing::Test {
+    Graph mkGraph(const V<pair<size_t, size_t>> &edges) {
+        size_t vertex_num = 0;
+        FORE (e, edges) {
+            vertex_num = max(vertex_num, max(e.first + 1, e.second + 1));
+        }
+
+        Graph g(vertex_num);
+        FORE (e, edges) {
+            g.add_edge({e.first, e.second});
+        }
+        return g;
+    }
+};
+
 typedef ::testing::Types<SimpleAdjacencyList, SimpleAdjacencyMatrix> Graphs;
 TYPED_TEST_CASE(DfsTest, Graphs);
+TYPED_TEST_CASE(BfsTest, Graphs);
 
 TYPED_TEST(DfsTest, SimpleTest) {
     std::vector<size_t> nodes;
@@ -61,6 +78,51 @@ TYPED_TEST(DfsTest, BreakTest) {
 TYPED_TEST(DfsTest, IsAddedTest) {
     std::vector<size_t> nodes;
     dfs_with_duplicate_vertices(this->mkGraph({{0, 1}, {0, 2}, {1, 2}}), {0}, [&](auto x) {
+        nodes.push_back(get<1>(x));
+        return false;
+    }, [](const auto& edge) { return get<1>(edge) != 2; });
+
+    EXPECT_EQ(1, nodes.size());
+    EXPECT_EQ(1, nodes[0]);
+}
+
+TYPED_TEST(BfsTest, SimpleTest) {
+    std::vector<size_t> nodes;
+    bfs_with_duplicate_vertices(this->mkGraph({{0, 1}, {1, 2}, {2, 3}, {1, 3}}), {0}, [&](auto x) {
+        nodes.push_back(get<1>(x));
+        return false;
+    }, [](const auto& edge) { return true; });
+
+    EXPECT_EQ(4, nodes.size());
+    EXPECT_EQ(1, nodes[0]);
+    EXPECT_EQ(2, nodes[1]);
+    EXPECT_EQ(3, nodes[2]);
+    EXPECT_EQ(3, nodes[3]);
+
+    nodes.clear();
+    bfs(this->mkGraph({{0, 1}, {0, 3}, {1, 2}, {3, 4}}), {0}, [&](auto x) {
+        nodes.push_back(get<1>(x));
+        return false;
+    }, [](const auto& edge) { return true; });
+
+    EXPECT_EQ(4, nodes.size());
+    EXPECT_TRUE(nodes[0] == 1 || nodes[0] == 3);
+    EXPECT_EQ((nodes[0] == 1) ? 3 : 1, nodes[1]);
+    EXPECT_TRUE(nodes[2] == 2 || nodes[2] == 4);
+    EXPECT_EQ((nodes[2] == 2) ? 4 : 2, nodes[3]);
+}
+TYPED_TEST(BfsTest, BreakTest) {
+    std::vector<size_t> nodes;
+    bfs_with_duplicate_vertices(this->mkGraph({{0, 1}, {1, 2}, {1, 3}}), {0}, [&](auto x) {
+        nodes.push_back(get<1>(x));
+        return get<1>(x) == 1;
+    }, [](const auto& edge) { return true; });
+
+    EXPECT_EQ(1, nodes.back());
+}
+TYPED_TEST(BfsTest, IsAddedTest) {
+    std::vector<size_t> nodes;
+    bfs_with_duplicate_vertices(this->mkGraph({{0, 1}, {0, 2}, {1, 2}}), {0}, [&](auto x) {
         nodes.push_back(get<1>(x));
         return false;
     }, [](const auto& edge) { return get<1>(edge) != 2; });
