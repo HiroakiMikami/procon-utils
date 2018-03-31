547e680a9b6a46f2a32d03876b7dbccdd3e67353
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-03-31 06:32:33 +0000

    Add C++ plugin

---
+++ languages/cc.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function create_newfile() {
+    set -u
+    local target=$1
+
+    local utils_base=${UTILS_BASE:-$(dirname $0)}
+    local build_dir=${BUILD_DIR:-./build}
+
+    mkdir -p $(dirname $target)
+    cat << EOF > $target
+/*
+URL https://
+SCORE 0
+AC false
+WA false
+TLE false
+MLE false
+TASK_TYPE
+FAILURE_TYPE
+NOTES
+*/
+EOF
+    cat $utils_base/languages/cc/templates/src/common.cc \
+        $utils_base/languages/cc/templates/src/main.cc   >> $target
+
+    # Update cmake build dir
+    local prev=$(pwd)
+    mkdir -p $build_dir
+    cd $build_dir
+    cmake ..
+    cd $prev
+}
+
+function execute() {
+    set -u
+    local path=$1
+    shift 1
+
+    local executable=$(echo ${path%.*} | tr "/" "-")
+    local build_dir=${BUILD_DIR:-./build}
+
+    # Build executable
+    make -C $build_dir $executable 1>&2
+
+    $@ $build_dir/src/$executable < /dev/stdin
+}
---
+++ languages/test/test_cc.bash
@@ -0,0 +1,52 @@
+#!/usr/bin/env bats
+
+UTILS_BASE=$(realpath ./)
+source scripts/util.bash
+source languages/cc.bash
+
+@test "create_newfile" {
+    tmpfile=$(mktemp)
+    workspace=$(mktemp -d)
+    mkdir -p $workspace/build
+    export BUILD_DIR=$workspace/build
+    touch $workspace/CMakeLists.txt
+    (create_newfile $tmpfile)
+
+    expected=$(mktemp)
+    cat << EOF > $expected
+/*
+URL https://
+SCORE 0
+AC false
+WA false
+TLE false
+MLE false
+TASK_TYPE
+FAILURE_TYPE
+NOTES
+*/
+EOF
+    cat $UTILS_BASE/languages/cc/templates/src/common.cc $UTILS_BASE/languages/cc/templates/src/main.cc >> $expected
+
+    [ "$(cat $tmpfile)" = "$(cat $expected)" ]
+
+    rm -rf $tmpfile
+    rm -rf $expected
+    rm -rf $workspace
+}
+@test "execute" {
+    BUILD_DIR=$(mktemp -d)
+
+    cat <<EOF > $BUILD_DIR/Makefile
+test:
+	mkdir -p $BUILD_DIR/src
+	echo "cat /dev/stdin" > $BUILD_DIR/src/test
+	chmod u+x $BUILD_DIR/src/test
+EOF
+
+    [ "$(echo xxx | execute test)" = "$(echo xxx)" ]
+
+    [ "$(echo xxx | execute test :)" = "" ]
+
+    rm -rf $BUILD_DIR
+}
